1780a3438798e55daad552e120bccd1e87e44052
Mike Lawrence <mike.lwrnc@gmail.com>
2014-01-08 20:42:02 -0400

    Added doBorder and windowColor start arguments, defaults for all start arguments. renamed end to quit.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 class stamperClass:
-	def start(self,windowSize,windowPosition):
+	def start(self,windowSize=(200,10),windowPosition=(0,0),windoColor=(255,255,255),doBorder=True):
 		import billiard
 		billiard.forking_enable(0)
 		self.qTo = billiard.Queue()
 		self.qFrom = billiard.Queue()
-		self.process = billiard.Process( target=stamperLoop , args=(windowSize,windowPosition,self.qTo,self.qFrom,) )
+		self.process = billiard.Process( target=stamperLoop , args=(windowSize,windowPosition,windowColor,doBorder,self.qTo,self.qFrom,) )
 		self.process.start()
 		return None
-	def end(self):
+	def quit(self):
 		self.qTo.put(['quit'])
 		self.process.terminate()
 		del self.qTo
@@ -15,7 +15,7 @@ class stamperClass:
 		return None
 
 
-def stamperLoop(windowSize,windowPosition,qTo,qFrom):
+def stamperLoop(windowSize,windowPosition,windowColor,doBorder,qTo,qFrom):
 	import sdl2
 	import sdl2.ext
 	import sys
@@ -27,10 +27,16 @@ def stamperLoop(windowSize,windowPosition,qTo,qFrom):
 	sdl2.SDL_Init(sdl2.SDL_INIT_TIMER)
 	timeFreq = 1.0/sdl2.SDL_GetPerformanceFrequency()
 	sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
-	window = sdl2.ext.Window("Timestamper", size= windowSize,position=windowPosition,flags=sdl2.SDL_WINDOW_SHOWN)
+	if doBorder:
+		flags = sdl2.SDL_WINDOW_SHOWN
+	else:
+		flags = sdl2.SDL_WINDOW_BORDERLESS | sdl2.SDL_WINDOW_SHOWN
+	window = sdl2.ext.Window("pyStamper",size=windowSize,position=windowPosition,flags=flags)
+	windowSurf = sdl2.SDL_GetWindowSurface(window.window)
+	sdl2.ext.fill(windowSurf.contents,sdl2.pixels.SDL_Color(r=windowColor[0], g=windowColor[1], b=windowColor[2], a=255))
 	window.refresh()
-	sdl2.SDL_Init(sdl2.SDL_INIT_JOYSTICK)
-	gamepad = sdl2.SDL_JoystickOpen(0)
+	#sdl2.SDL_Init(sdl2.SDL_INIT_JOYSTICK) #uncomment if you want joystick input
+	#sdl2.SDL_JoystickOpen(0) #uncomment if you want joystick input
 	while True:
 		sdl2.SDL_PumpEvents()
 		if not qTo.empty():
